eeff5bb86857ce67a833ce2718beaff5e5db4673
GokuSuperSaiyan72 <KunalRajput7656@gmail.com>
2024-07-15 16:28:19 +0530

    chore: added new endpoint in constants

--- constants.js
+++ constants.js
@@ -1,4 +1,7 @@
-const ENDPOINTS = ["https://opportunex-backend-1.onrender.com"];
+const ENDPOINTS = [
+  "https://keep-server-alive.onrender.com",
+  "https://opportunex-backend-1.onrender.com",
+];
 const PORT = 5500;
 const CRON_EXPRESSION = "*/10 * * * *";
 
