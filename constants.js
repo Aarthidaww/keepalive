62d519bd323934bc7da6e8b97fe176685cbc6c6f
GokuSuperSaiyan72 <KunalRajput7656@gmail.com>
2024-08-19 08:10:43 +0530

    fix: update ping-endpoint URLS

--- constants.js
+++ constants.js
@@ -1,6 +1,7 @@
 const ENDPOINTS = [
-  "https://keep-server-alive-qa4c.onrender.com",
-  "https://opportunex-backend-8spz.onrender.com",
+  "https://keep-server-alive-sbx8.onrender.com",
+  "https://opportunex-backend-2p67.onrender.com",
+  "https://ignouteeru-service-xwam.onrender.com",
 ];
 const PORT = 5500;
 const CRON_EXPRESSION = "*/10 * * * *";
