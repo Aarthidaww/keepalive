d17665d138d31493c0729d6eb41a296de23fba2d
Aarthidaww <142527321+Aarthidaww@users.noreply.github.com>
2025-04-25 19:09:07 +0800

    electrosweet.onrender.com/

--- constants.js
+++ constants.js
@@ -1,7 +1,5 @@
 const ENDPOINTS = [
-  "https://keep-server-alive-sbx8.onrender.com",
-  "https://opportunex-backend-2p67.onrender.com",
-  "https://ignouteeru-service-xwam.onrender.com",
+  "https://electrosweet.onrender.com/",
 ];
 const PORT = 5500;
 const CRON_EXPRESSION = "*/10 * * * *";
